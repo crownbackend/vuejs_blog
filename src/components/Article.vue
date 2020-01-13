1693c76c8787e20db05961219f36897ea348d815
crownbackend <crownbackend@gmail.com>
2020-01-13 21:25:02 +0100

    register form second

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
     <div class="uk-grid" data-ukgrid="">
-      <div class="uk-width-2-3@m">
-        <h4 class="uk-heading-line uk-text-bold"><span>Latest News</span></h4>
+      <div class="uk-width-2-3@m" v-for="article in articles" v-bind:key="article.id">
+        <h4 class="uk-heading-line uk-text-bold"><span>{{ article.category.name }}</span></h4>
         <article class="uk-section uk-section-small uk-padding-remove-top">
           <div>
-            <h2 class="uk-margin-remove-adjacent uk-text-bold uk-margin-small-bottom"><a title="Fusce facilisis tempus magna ac dignissim." class="uk-link-reset" href="#">Fusce facilisis tempus magna ac dignissim.</a></h2>
-            <p class="uk-article-meta">Written on March 23, 2019. Posted in <a href="#">Blog</a> | <span data-uk-icon="icon: future" class="uk-icon"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="future"><polyline points="19 2 18 2 18 6 14 6 14 7 19 7 19 2"></polyline><path fill="none" stroke="#000" stroke-width="1.1" d="M18,6.548 C16.709,3.29 13.354,1 9.6,1 C4.6,1 0.6,5 0.6,10 C0.6,15 4.6,19 9.6,19 C14.6,19 18.6,15 18.6,10"></path><rect x="9" y="4" width="1" height="7"></rect><path d="M13.018,14.197 L9.445,10.625" fill="none" stroke="#000" stroke-width="1.1"></path></svg></span> Takes 7 min reading.</p>
+            <h2 class="uk-margin-remove-adjacent uk-text-bold uk-margin-small-bottom"><a title="Fusce facilisis tempus magna ac dignissim." class="uk-link-reset" href="#">{{ article.title }}</a></h2>
+            <p class="uk-article-meta">Article publié le : {{format_date(article.created_at.date)}}</p>
           </div>
           <figure>
-            <img src="https://picsum.photos/800/300/?random=1" data-src="https://picsum.photos/800/300/?random=1" width="800" height="300" alt="Alt text" class="lazy" data-uk-img="">
+            <img  v-bind:src="article.image_name" v-bind:data-src="article.image_name" width="800" height="300" v-bind:alt="article.image_name" class="lazy" data-uk-img="">
             <figcaption class="uk-padding-small uk-text-center uk-text-muted">Caption of the image</figcaption>
           </figure>
-          <p>UPDATE 24th October 15.10 BST&#8202;—&#8202;Vivamus sed consequat urna. Fusce vitae urna sed ante placerat iaculis. Suspendisse potenti. Pellentesque quis fringilla libero. In hac habitasse platea dictumst.</p>
-          <p>Ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-          <a href="#" title="Read More" class="uk-button uk-button-default uk-button-small">READ MORE</a>
+          <p>
+            {{ article.description}}
+          </p>
+          <a href="#" title="Read More" class="uk-button uk-button-default uk-button-small">Lire l'article</a>
           <hr>
         </article>
 
@@ -26,16 +27,6 @@
           <li><a href="">February</a></li>
         </ul>
       </div>
-    </div>
-    <br>
-    <br>
-    <br>
-
-    <div v-for="article in articles" v-bind:key="article.id">
-      <h1>
-        {{article.title}}
-      </h1>
-
     </div>
   </div>
 </template>
@@ -51,8 +42,22 @@ export default {
   },
   mounted() {
     axios
-    .get('https://127.0.0.1:8000/')
-    .then(response => (this.articles = response))
+    .get('https://127.0.0.1:8000/api/')
+    .then(response => (this.articles = response.data))
+  },
+  methods: {
+    format_date(value) {
+      if(value) {
+        const d = new Date(value);
+        let year = d.getFullYear();
+        let month = (1 + d.getMonth()).toString().padStart(2, '0');
+        let day = d.getDate().toString().padStart(2, '0');
+        let hour = d.getHours();
+        let minute = d.getMinutes()
+
+        return month + '/' + day + '/' + year +' à '+ hour +':'+ (minute<10?'0':'') + minute;
+      }
+    }
   }
 }
 </script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,15 +1,126 @@
 <template>
   <div>
-    register
+    <section class="uk-section uk-section-xsmall" uk-height-viewport="offset-top: true">
+      <div class="uk-container">
+        <div class="uk-flex uk-flex-center">
+          <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-width-large">
+            <h3>Register</h3>
+            <form @submit.prevent="checkForm" class="uk-form-stacked" method="post">
+              <div class="uk-margin">
+                <label class="uk-form-label" for="username">
+                  Username
+                </label>
+                <div class="uk-form-controls">
+                  <div class="uk-inline uk-width-1-1">
+                    <a class="uk-form-icon uk-form-icon-flip" uk-tooltip="title: Alpha-numeric characters.<br>Minimum 3 characters.<br>Maximum 16 characters.<br>This field is required.; pos: bottom-right"><i class="fas fa-info-circle"></i></a>
+                    <input class="uk-input" id="username" name="username" v-model="username" type="text">
+                  </div>
+                  <div v-if="errorUsername" class="error">
+                    {{errorUsername}}
+                  </div>
+                </div>
+              </div>
+              <div class="uk-margin">
+                <label class="uk-form-label" for="email">
+                  Email
+                </label>
+                <div class="uk-form-controls">
+                  <div class="uk-inline uk-width-1-1">
+                    <a class="uk-form-icon uk-form-icon-flip" uk-tooltip="title: Must be a valid email.<br>One account per email.<br>This field is required.; pos: bottom-right"><i class="fas fa-info-circle"></i></a>
+                    <input class="uk-input" id="email" v-model="email" name="email" type="text">
+                  </div>
+                  <div v-if="errorEmail" class="error">
+                    {{errorEmail}}
+                  </div>
+                </div>
+              </div>
+              <div class="uk-margin">
+                <label class="uk-form-label" for="password">
+                  Password
+                </label>
+                <div class="uk-form-controls">
+                  <div class="uk-inline uk-width-1-1">
+                    <a class="uk-form-icon uk-form-icon-flip" uk-tooltip="title: Minimum 8 characters.<br>This field is required.; pos: bottom-right"><i class="fas fa-info-circle"></i></a>
+                    <input class="uk-input" id="password" v-model="password" name="password" type="password">
+                  </div>
+                  <div v-if="errorPassword" class="error">
+                    {{errorPassword}}
+                  </div>
+                </div>
+              </div>
+<!--              <div class="uk-margin">-->
+<!--                <label class="uk-form-label" for="repeat_password">-->
+<!--                  Repeat Password-->
+<!--                </label>-->
+<!--                <div class="uk-form-controls">-->
+<!--                  <div class="uk-inline uk-width-1-1">-->
+<!--                    <a class="uk-form-icon uk-form-icon-flip" uk-tooltip="title: Must match the password field.<br>This field is required.; pos: bottom-right"><i class="fas fa-info-circle"></i></a>-->
+<!--                    <input class="uk-input" id="repeat_password" name="repeat_password" type="password">-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+              <div class="uk-margin">
+                <label>
+                  <input class="uk-checkbox" name="terms_of_service" type="checkbox"> I accept the <a href="">terms of service</a>.
+                </label>
+              </div>
+              <button class="uk-button uk-button-primary uk-width-1-1" type="submit">Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
 export default {
-  name: "register"
+  name: "register",
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      errorUsername: '',
+      errorEmail: '',
+      errorPassword: '',
+
+    }
+  },
+  methods: {
+    checkForm: function () {
+      if(!this.username) {
+        this.errorUsername = 'Username est obligatoire';
+      }
+      if(!this.email) {
+        this.errorEmail = 'Email est obligatoire';
+      }
+      if(!this.password) {
+        this.errorPassword = 'Le mot de passe est obligatoire';
+      }
+
+      if(this.username && this.email && this.password) {
+        this.errorUsername = '';
+        this.errorEmail = '';
+        this.errorPassword = '';
+        Axios.post('https://127.0.0.1:8000/api/register',{
+          "username": this.username,
+          "email": this.email,
+          "password": this.password,
+          "Content-Type": "application/x-www-form-urlencoded"
+        }).then(response => (this.errorEmail = response.data.email[0]))
+      }
+    }
+  }
+
 }
 </script>
 
 <style>
-
+.error {
+  color: red;
+  padding-top: 20px;
+}
 </style>
+
